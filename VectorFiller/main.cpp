1adec1e6c2d3a95244d1542d18cfa656569b0cd4
Jo Wagner <jowagner@limago.de>
2026-03-26 08:49:47 +0100

    initial commit

--- VectorFiller/main.cpp
+++ VectorFiller/main.cpp
@@ -1,6 +1,20 @@
+#include <chrono>
+#include <cstdint>
 #include <iostream>
+#include <memory>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const size_t collection_size = INT32_MAX / 2;
+
+    auto feld = std::make_shared<std::vector<int>>(collection_size);
+
+    std::cout << "Running..." << std::endl;
+    auto start_ = std::chrono::high_resolution_clock::now();
+    for (int  i = 0; i < collection_size ; ++i) {
+        feld->at(i) = rand();
+    }
+    auto end_ = std::chrono::high_resolution_clock::now();
+    std::cout << "Duration: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_-start_).count() << std::endl;
     return 0;
 }
